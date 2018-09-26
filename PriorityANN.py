1c588817175d0c4fa3f7f3f97734aa46cf10cfa2
Dion Lao <dion.lao@uqconnect.edu.au>
2018-09-26 21:11:31 +0800

    Added Tensorflow prep codes

--- PriorityANN.py
+++ PriorityANN.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import csv
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 class PriorityANN:
     """ANN to use for determining priority listing"""
@@ -14,8 +16,27 @@ class PriorityANN:
 
         self.passengerList = self._getPassengerDetails("SQ890")
 
-        self.writeCSV("passengerDetails.csv")
-        print(self.passengerList)
+        #self.writeCSV("passengerDetails.csv")
+        
+#        mnist = tf.keras.datasets.mnist
+#        print(mnist.load_data()[0][0].shape)
+		
+#        (x_train, y_train),(x_test, y_test) = mnist.load_data()
+#        x_train, x_test = x_train / 255.0, x_test / 255.0
+#		
+#        model = tf.keras.models.Sequential([
+#			  tf.keras.layers.Flatten(),
+#			  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+#			  tf.keras.layers.Dropout(0.2),
+#			  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+#			  ])
+#        model.compile(optimizer='adam',
+#		              loss='sparse_categorical_crossentropy',
+#		              metrics=['accuracy'])
+#		
+#        model.fit(x_train, y_train, epochs=5)
+#        model.evaluate(x_test, y_test)
+
 
 
     def requestData(self, url, payload):
@@ -61,3 +82,10 @@ class PriorityANN:
                 tempPassenger[param] = passenger.get(param)
             passengerList[idx] = tempPassenger
         return passengerList
+    
+        
+        
+        
+        
+        
+        
