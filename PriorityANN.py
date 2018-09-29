de2b5d3df4213da6f40be96d35b51c83d62d13b0
Dion Lao <dion.lao@uqconnect.edu.au>
2018-09-29 13:34:13 +0800

    Added user prompts

--- PriorityANN.py
+++ PriorityANN.py
@@ -2,6 +2,7 @@ import requests
 import json
 import csv
 import urllib
+import pandas as pd
 
 class PriorityANN:
     """ANN to use for determining priority listing"""
@@ -9,27 +10,36 @@ class PriorityANN:
         print("Initialising...")
         self.desiredParameters = ['firstName', 'lastName', 'bookingClass', 'email', 'checkInStatus', 'willingEvictor']
         self.passengerList = []
+        self.flightList = ["SQ494", "SQ890"]
 
     def setFlight(self, flightNo):
         self.passengerList = self._getPassengerDetails(flightNo)
-        self.writeCSV("passengerDetails.csv")
+        #self.writeCSV("passengerDetails.csv")
 
-    def train(self, api_url):
+    def buildCSV(self):
         # do nothing
         print("nothing")
 
 
-    def evaluate(self):
+    def evaluate(self, flightNo):
+        passengerList = self._getPassengerDetails(flightNo)
+        values = []
+        columnNames = ["firstName", "lastName", "bookingClass", "KFTier", "email", "checkInStatus",
+                                        "willingEvictor"]
+        for passenger in passengerList:
+            passengerInfo = []
+            for columnName in columnNames:
+                passengerInfo.append(passenger.get(columnName))
+            values.append(passengerInfo)
+
+
         data = {
 
             "Inputs": {
-
                 "input1":
                     {
-                        "ColumnNames": ["firstName", "lastName", "bookingClass", "KFTier", "email", "checkInStatus",
-                                        "willingEvictor"],
-                        "Values": [["Dion", "Lao", "Business", "EGTP", "dion_lao@hotmail.com", "Checked In", "0"],
-                                   ["Hello", "Mate", "Economy", "EGTP", "coolios@hotmail.com", "Checked In", "0"], ]
+                        "ColumnNames": columnNames,
+                        "Values": values
                     }, },
             "GlobalParameters": {
             }
@@ -46,12 +56,24 @@ class PriorityANN:
         try:
             response = urllib.request.urlopen(req)
 
-            # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
-            # req = urllib.request.Request(url, body, headers)
-            # response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode('utf-8'))
+            scoreList = result["Results"]["output1"]["value"]["Values"]
+            passengerNames = []
+            scoreListParsed = []
+            for passengerIdx, score in enumerate(scoreList):
+                firstName = passengerList[passengerIdx].get('firstName')
+                lastName = passengerList[passengerIdx].get('lastName')
+                passengerNames.append(firstName + " " + lastName)
+                scoreListParsed.append(float(score[0]))
 
-            result = response.read()
-            print(result)
+            score_df = pd.DataFrame({
+                'score' : scoreListParsed,
+                'passengerName' : passengerNames
+            })
+            sorted_score_df = score_df.sort_values(by=['score'], ascending=False)
+            print(sorted_score_df)
+
+
         except urllib.error.HTTPError:
             print("The request failed with status code: ")
 
@@ -88,21 +110,4 @@ class PriorityANN:
             passenger["willingEvictor"] = False if passenger.get("bookingClass") == "Business" else True
 
         return passengerList
-
-    def _getBaggageDetails(self):
-
-        url = "https://apigw.singaporeair.com/appchallenge/api/flightroutestatus"
-
-        payload = "{\"originAirportCode\": \"SIN\", \"destinationAirportCode\": \"DXB\", \"scheduledDepartureDate\": \"2018-08-15\"}"
-        headers = {
-            'content-type': "application/json",
-            'apikey': "aghk73f4x5haxeby7z24d2rc",
-            'cache-control': "no-cache",
-            'postman-token': "9a6ab98c-9f7f-cf6d-d081-100676469fad"
-        }
-
-        response = requests.request("POST", url, data=payload, headers=headers)
-
-        print(response.text)
-        return response.text
 
--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from PriorityANN import PriorityANN
 
 def main():
     priorityANN = PriorityANN()
-    # while True:
-    #     flightNo = input("Flight No: ")
-    #     print("Setting flight No "+flightNo+"...")
-    priorityANN.setFlight("SQ890")
-    print(priorityANN.passengerList)
-    priorityANN.evaluate()
+    while True:
+        command = input("What would you like to do?\n [C = Build CSV from current flights to train] \n [E = Evict passengers from flight] \n")
+        if command.upper() == "C":
+            print("Generating CSV")
+            generateCSVModel(priorityANN)
+            print("CSV generated and stored in passengerDetails.csv")
+        elif command.upper() == "E":
+            showFlightEvictors(priorityANN)
+        else:
+            print("Sorry that is not an option")
 
+    priorityANN.setFlight("SQ890")
+    priorityANN.evaluate("SQ890")
+
+def generateCSVModel(priorityANN):
+    priorityANN.setFlight("SQ890")
+    priorityANN.writeCSV("passengerDetails.csv")
+
+def showFlightEvictors(priorityANN):
+    flightNo = input("Which flight would you like to select? \n The available flights are [SQ890]\n").upper()
+    while flightNo not in ["SQ890"]:
+        print("Sorry, flight "+ flightNo + " does not exist.")
+        flightNo = input("Which flight would you like to select? \n The available flights are [SQ890]\n")
+    priorityANN.evaluate(flightNo)
 
 if __name__ == "__main__":
     main()
